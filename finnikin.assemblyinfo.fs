e0e4e81deb5b3147c60f5efaca4e0d7824430f3c
Onorio Catenacci <catenacci@ieee.org>
2012-01-13 15:18:51 -0500

    Added assembly info and modified namespace/module declarations

---
+++ finnikin.assemblyinfo.fs
@@ -0,0 +1,15 @@
+namespace finnikin
+    open System
+    open System.Reflection
+    open System.Runtime.CompilerServices
+    open System.Runtime.InteropServices
+
+    [<assembly: ComVisible (false)>]
+    [<assembly: CLSCompliant (false)>]
+    [<assembly: AssemblyTitle ("Finnikin")>]
+    [<assembly: AssemblyDescription ("Finnikin is an F# library to ease with the creation of FSX scripts")>]
+    [<assembly: AssemblyProduct ("Finnikin")>]
+    [<assembly: AssemblyVersion ("0.1.*")>]
+    [<assembly: AssemblyFileVersion ("0.1.*")>]
+    [<assembly: AssemblyDelaySign (false)>]
+    do ()
