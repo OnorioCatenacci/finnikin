b3499e8e0f1ca61756e0106b1b1fe44ef2936ef4
Onorio Catenacci <catenacci@ieee.org>
2012-02-09 22:22:37 -0500

    Added a few more unit tests and modified the unit test names to take advantage of F#'s ability to have spaces in function names.

--- finnikin.test/finnikin.test.fs
+++ finnikin.test/finnikin.test.fs
@@ -8,22 +8,39 @@
 
     module Test =
         [<Test>]
-        let ``TestGetEnvironmentVariable``() =
+        let ``Test Get Good Environment Variable``() =
             Assert.IsTrue(Option.isSome(Main.GetEnvironmentVariable "PATH"))
 
         [<Test>]
-        let ``TestGetNonExistentEnvironmentVariable``() =
+        let ``Test Get NonExistent Environment Variable``() =
             Assert.IsTrue(Option.isNone(Main.GetEnvironmentVariable "NonExistent"))
          
         [<Test>]
-        let ``TestGetEnvironmentVariableWithEmptyName``() =
+        let ``Test Get Environment Variable With Empty Name``() =
             Assert.IsTrue(Option.isNone(Main.GetEnvironmentVariable ""))     
         
         [<Test>]
-        let ``TestDoNotOverwriteExistingEnvironmentVariable``() =
+        let ``Test Do Not Overwrite Existing Environment Variable``() =
             let setEnvVarResult = Main.SetEnvironmentVariable false "PATH" "P1"
             Assert.AreEqual(Finnikin.opResult.Failure,setEnvVarResult)
 
+        [<Test>]
+        let ``Test Deliberate Overwrite Of Existing Environment Variable``() =
+            let currentPath = Main.GetEnvironmentVariable "PATH"
+            let cpString = currentPath.Value
+            let setEnvVarResult = Main.SetEnvironmentVariable true "PATH" (cpString + ";P1")
+            Assert.AreEqual(Finnikin.opResult.Success,setEnvVarResult)
+            let modifiedCP = Main.GetEnvironmentVariable "PATH"
+            StringAssert.AreEqualIgnoringCase(cpString + ";P1",modifiedCP.Value)
+            //Set the variable back to the original value
+            Main.SetEnvironmentVariable true "PATH" cpString |> ignore
+
+        [<Test>]
+        let ``Attempt To Overwrite NonExistent Environment Variable``() =
+            let setEnvVarResult = Main.SetEnvironmentVariable true "DOESNOTEXIST" "DOESNOTMATTER"
+            //Should succeed because it should create the environment variable and set it
+            Assert.AreEqual(Finnikin.opResult.Success,setEnvVarResult)
+        
         [<EntryPoint>]
         let main args =
             0
