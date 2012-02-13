0294afdda011a3a8383e4b470155cf9031d9ca42
Onorio Catenacci <catenacci@ieee.org>
2012-02-13 12:30:32 -0500

    v0.3.0
    1.) Remedied the need to use the "Main" module which annoyed me intensely.
    
    2.) Added code to check for Linux or MacOSX both of which are case-sensitive for
    environment variables.
    
    	deleted:    finnikin.assemblyinfo.fs
    	deleted:    finnikin.fs
    	deleted:    finnikin.proj
    	modified:   finnikin.test/finnikin.test.fs
    	modified:   finnikin/finnikin.fs

--- finnikin.test/finnikin.test.fs
+++ finnikin.test/finnikin.test.fs
@@ -3,43 +3,45 @@
  *
  *)
 
- namespace Finnikin
-    open NUnit.Framework
+// namespace Finnikin
 
-    module Test =
+    module Finnikin.Test 
+        open NUnit.Framework
+        open Finnikin
+
         [<Test>]
         let ``Test Get Good Environment Variable``() =
-            Assert.IsTrue(Option.isSome(Main.GetEnvironmentVariable "PATH"))
+            Assert.IsTrue(Option.isSome(Finnikin.GetEnvironmentVariable "PATH"))
 
         [<Test>]
         let ``Test Get NonExistent Environment Variable``() =
-            Assert.IsTrue(Option.isNone(Main.GetEnvironmentVariable "NonExistent"))
+            Assert.IsTrue(Option.isNone(Finnikin.GetEnvironmentVariable "NonExistent"))
          
         [<Test>]
         let ``Test Get Environment Variable With Empty Name``() =
-            Assert.IsTrue(Option.isNone(Main.GetEnvironmentVariable ""))     
+            Assert.IsTrue(Option.isNone(Finnikin.GetEnvironmentVariable ""))     
         
         [<Test>]
         let ``Test Do Not Overwrite Existing Environment Variable``() =
-            let setEnvVarResult = Main.SetEnvironmentVariable false "PATH" "P1"
-            Assert.AreEqual(Finnikin.opResult.Failure,setEnvVarResult)
+            let setEnvVarResult = Finnikin.SetEnvironmentVariable false "PATH" "P1"
+            Assert.AreEqual(false,setEnvVarResult)
 
         [<Test>]
         let ``Test Deliberate Overwrite Of Existing Environment Variable``() =
-            let currentPath = Main.GetEnvironmentVariable "PATH"
+            let currentPath = Finnikin.GetEnvironmentVariable "PATH"
             let cpString = currentPath.Value
-            let setEnvVarResult = Main.SetEnvironmentVariable true "PATH" (cpString + ";P1")
-            Assert.AreEqual(Finnikin.opResult.Success,setEnvVarResult)
-            let modifiedCP = Main.GetEnvironmentVariable "PATH"
+            let setEnvVarResult = Finnikin.SetEnvironmentVariable true "PATH" (cpString + ";P1")
+            Assert.AreEqual(true,setEnvVarResult)
+            let modifiedCP = Finnikin.GetEnvironmentVariable "PATH"
             StringAssert.AreEqualIgnoringCase(cpString + ";P1",modifiedCP.Value)
             //Set the variable back to the original value
-            Main.SetEnvironmentVariable true "PATH" cpString |> ignore
+            Finnikin.SetEnvironmentVariable true "PATH" cpString |> ignore
 
         [<Test>]
         let ``Attempt To Overwrite NonExistent Environment Variable``() =
-            let setEnvVarResult = Main.SetEnvironmentVariable true "DOESNOTEXIST" "DOESNOTMATTER"
+            let setEnvVarResult = Finnikin.SetEnvironmentVariable true "DOESNOTEXIST" "DOESNOTMATTER"
             //Should succeed because it should create the environment variable and set it
-            Assert.AreEqual(Finnikin.opResult.Success,setEnvVarResult)
+            Assert.AreEqual(true,setEnvVarResult)
         
         [<EntryPoint>]
         let main args =
--- finnikin/finnikin.fs
+++ finnikin/finnikin.fs
@@ -2,165 +2,160 @@
 * Finnikin
 * Onorio Catenacci
 * Finnikin is an F# library to make certain shell operations easier.  Since this is intended to be used from F#, usability from other .Net languages is not a high priority issue.
-* v0.2.0 7 February 2012
+* v0.3.0 13 February 2012
 *)
 
-///<summary>
-///The Finnikin namespace
-///</summary>
-namespace Finnikin 
+module Finnikin
     open System
     open System.Diagnostics
     open System.IO
     open System.Security
     open System.ServiceProcess
-
-    type opResult =
-        public  
-        | Success
-        | SuccessWithMessage
-        | Failure
- 
-    module Main =
     
-        ///<summary>
-        /// Returns an option containing the definition of the environment string if it exists or None otherwise.  NB at least on Mac OSX environment variables are case-sensitive.
-        /// That is, path <> PATH and the code doesn't automatically test for all possible cases.
-        ///</summary>
-        ///<param name="envVarName">Environment variable name to get the definition of</param>
-        let GetEnvironmentVariable envVarName =
-            try
-                let envVar = Environment.GetEnvironmentVariable(envVarName)
-                match envVar with
-                | null -> None
-                | _ -> Some(envVar)
-            with
-            | :? SecurityException -> None
-
-        ///<summary>
-        ///Set an environment variable.  The routine checks to see if the environment variable is already set; user passes in a parameter to indicate whether or not the existing
-        ///value should be overwritten.
-        ///</summary>
-        ///<param name="envVarName">Environment variable to set definition for</param>
-        ///<param name="envVarValue">Value to set specified environment variable to</param>
-        ///<param name="overwrite">If the environment variable is already set should the value be overwritten</param>
-        let SetEnvironmentVariable shouldOverwrite environmentVariableName environmentVariableValue =
-            if (not shouldOverwrite && Option.isSome(GetEnvironmentVariable environmentVariableName)) then
-                Failure
-            else
-                try
-                    Environment.SetEnvironmentVariable (environmentVariableName, environmentVariableValue)
-                    Success
-                with
-                | :? SecurityException -> Failure        
-        
-        ///<summary>
-        /// Returns a sequence containing all the files which match the given spec.  User specifies starting dir and also whether or not they want to recurse to subdirs
-        ///</summary>
-        ///<param name="baseDir">Directory to start looking for files in</param>
-        ///<param name="fileSpec">File mask to match</param>
-        ///<param name="recurse">Should the process recurse into subdirectories or not</param>         
-        let GetFileList baseDir fileSpec recurse =
-            try
-                if Directory.Exists(baseDir) then
-                    let rec getAllFiles baseDir fileSpec =
-                        seq{
-                            yield! Directory.EnumerateFiles(baseDir, fileSpec)
-                            if recurse then
-                                for d in Directory.EnumerateDirectories(baseDir) do
-                                    yield! getAllFiles d fileSpec
-                        }
-                    getAllFiles baseDir fileSpec           
+    ///<summary>
+    /// Returns an option containing the definition of the environment string if it exists or None otherwise.  NB at least on Mac OSX environment variables are case-sensitive.
+    /// That is, path <> PATH and the code doesn't automatically test for all possible cases.
+    ///</summary>
+    ///<param name="envVarName">Environment variable name to get the definition of</param>
+    let GetEnvironmentVariable (envVarName:string) =
+        try
+            //We need to allow for the fact that we may be on a Mono box.
+            let correctedEnvVarName = 
+                if (Environment.OSVersion.Platform = PlatformID.Unix || Environment.OSVersion.Platform = PlatformID.MacOSX) then
+                    envVarName.ToUpper()
                 else
-                    Seq.empty
-            with
-            | :? SecurityException | :? UnauthorizedAccessException -> Seq.empty
+                    envVarName
+            let envVar = Environment.GetEnvironmentVariable(correctedEnvVarName)
+            match envVar with
+            | null -> None
+            | _ -> Some(envVar)
+        with
+        | :? SecurityException -> None
 
-        ///<summary>
-        /// Applies the specified function to all files which match the given spec
-        ///</summary>
-        ///<param name="baseDir">Directory to start looking for files in</param>
-        ///<param name="fileSpec">File mask to match</param>
-        ///<param name="recurse">Should the process recurse into subdirectories or not</param>         
-        let FileOperation baseDir fileSpec recurse functionToApply =
-            GetFileList baseDir fileSpec recurse |> Seq.iter functionToApply
-
-        ///<summary>
-        ///Touch a specified file or set of files to modify the datetime for purposes of building
-        ///</summary>
-        ///<param name="baseDir">Directory to start looking for files in</param>
-        ///<param name="fileSpec">File mask to match</param>
-        ///<param name="recurse">Should the operation recurse into subdirectories or not</param>
-        let Touch baseDir fileSpec recurse =
-            FileOperation baseDir fileSpec recurse (fun f -> FileInfo(f).LastAccessTime <- DateTime.Now) 
-
-        ///<summary>
-        /// Either returns the named service or returns an indication that the named service was not found
-        ///</summary>
-        ///<param name="serviceName">The service to find</param>
-        let GetService serviceName =
+    ///<summary>
+    ///Set an environment variable.  The routine checks to see if the environment variable is already set; user passes in a parameter to indicate whether or not the existing
+    ///value should be overwritten.
+    ///</summary>
+    ///<param name="envVarName">Environment variable to set definition for</param>
+    ///<param name="envVarValue">Value to set specified environment variable to</param>
+    ///<param name="overwrite">If the environment variable is already set should the value be overwritten</param>
+    let SetEnvironmentVariable shouldOverwrite environmentVariableName environmentVariableValue =
+        if (not shouldOverwrite && Option.isSome(GetEnvironmentVariable environmentVariableName)) then
+            false
+        else
             try
-                let servicesList = ServiceController.GetServices()
-                let s = servicesList |> Array.find (fun s -> s.ServiceName = serviceName)
-                (true,Some(s))
+                Environment.SetEnvironmentVariable (environmentVariableName, environmentVariableValue)
+                true
             with
-            | :? System.ComponentModel.Win32Exception | :? System.Collections.Generic.KeyNotFoundException -> (false,None) 
+            | :? SecurityException -> false        
+        
+    ///<summary>
+    /// Returns a sequence containing all the files which match the given spec.  User specifies starting dir and also whether or not they want to recurse to subdirs
+    ///</summary>
+    ///<param name="baseDir">Directory to start looking for files in</param>
+    ///<param name="fileSpec">File mask to match</param>
+    ///<param name="recurse">Should the process recurse into subdirectories or not</param>         
+    let GetFileList baseDir fileSpec recurse =
+        try
+            if Directory.Exists(baseDir) then
+                let rec getAllFiles baseDir fileSpec =
+                    seq{
+                        yield! Directory.EnumerateFiles(baseDir, fileSpec)
+                        if recurse then
+                            for d in Directory.EnumerateDirectories(baseDir) do
+                                yield! getAllFiles d fileSpec
+                    }
+                getAllFiles baseDir fileSpec           
+            else
+                Seq.empty
+        with
+        | :? SecurityException | :? UnauthorizedAccessException -> Seq.empty
 
-        ///<summary>
-        /// Check if a service with the specified service name is running or not
-        ///</summary>
-        ///<param name="serviceName">Service to test</param>
-        let IsServiceRunning serviceName =
-            try
-                let (serviceExists,service) = GetService serviceName
-                if serviceExists then
-                    service.Value.Status = System.ServiceProcess.ServiceControllerStatus.Running
-                else 
-                    false            
-            with
-            | :? System.ArgumentException -> false
+    ///<summary>
+    /// Applies the specified function to all files which match the given spec
+    ///</summary>
+    ///<param name="baseDir">Directory to start looking for files in</param>
+    ///<param name="fileSpec">File mask to match</param>
+    ///<param name="recurse">Should the process recurse into subdirectories or not</param>         
+    let FileOperation baseDir fileSpec recurse functionToApply =
+        GetFileList baseDir fileSpec recurse |> Seq.iter functionToApply
+
+    ///<summary>
+    ///Touch a specified file or set of files to modify the datetime for purposes of building
+    ///</summary>
+    ///<param name="baseDir">Directory to start looking for files in</param>
+    ///<param name="fileSpec">File mask to match</param>
+    ///<param name="recurse">Should the operation recurse into subdirectories or not</param>
+    let Touch baseDir fileSpec recurse =
+        FileOperation baseDir fileSpec recurse (fun f -> FileInfo(f).LastAccessTime <- DateTime.Now) 
+
+    ///<summary>
+    /// Either returns the named service or returns an indication that the named service was not found
+    ///</summary>
+    ///<param name="serviceName">The service to find</param>
+    let GetService serviceName =
+        try
+            let servicesList = ServiceController.GetServices()
+            let s = servicesList |> Array.find (fun s -> s.ServiceName = serviceName)
+            (true,Some(s))
+        with
+        | :? System.ComponentModel.Win32Exception | :? System.Collections.Generic.KeyNotFoundException -> (false,None) 
+
+    ///<summary>
+    /// Check if a service with the specified service name is running or not
+    ///</summary>
+    ///<param name="serviceName">Service to test</param>
+    let IsServiceRunning serviceName =
+        try
+            let (serviceExists,service) = GetService serviceName
+            if serviceExists then
+                service.Value.Status = System.ServiceProcess.ServiceControllerStatus.Running
+            else 
+                false            
+        with
+        | :? System.ArgumentException -> false
             
 
-        ///<summary>
-        /// Attempt to start the named service on this machine. Note that no indication of success or failure is given
-        ///</summary>
-        ///<param name="serviceName">Service to attempt to start</param>
-        let StartService serviceName =
-            let (serviceExists,service) = GetService serviceName
-            if serviceExists && not(IsServiceRunning(service.Value.ServiceName)) then
-                service.Value.Start()
+    ///<summary>
+    /// Attempt to start the named service on this machine. Note that no indication of success or failure is given
+    ///</summary>
+    ///<param name="serviceName">Service to attempt to start</param>
+    let StartService serviceName =
+        let (serviceExists,service) = GetService serviceName
+        if serviceExists && not(IsServiceRunning(service.Value.ServiceName)) then
+            service.Value.Start()
 
-        ///<summary>
-        /// Attempt to stop the named service on this machine. Note that no indication of success or failure is given
-        ///</summary>
-        ///<param name="serviceName">Service to attempt to stop</param>
-        let StopService serviceName =
-            let (serviceExists,service) = GetService serviceName
-            if serviceExists && (IsServiceRunning(service.Value.ServiceName)) && service.Value.CanStop then
-                service.Value.Stop()
+    ///<summary>
+    /// Attempt to stop the named service on this machine. Note that no indication of success or failure is given
+    ///</summary>
+    ///<param name="serviceName">Service to attempt to stop</param>
+    let StopService serviceName =
+        let (serviceExists,service) = GetService serviceName
+        if serviceExists && (IsServiceRunning(service.Value.ServiceName)) && service.Value.CanStop then
+            service.Value.Stop()
 
-        ///<summary>
-        ///Set all files and directories which match a given spec to read and write
-        ///</summary>
-        ///<param name="baseDir">Directory to start looking for files in</param>
-        ///<param name="fileSpec">File mask to match</param>
-        ///<param name="recurse">Should the operation recurse into subdirectories or not</param>
-        let SetFilesToReadWrite baseDir fileSpec recurse =
-            FileOperation baseDir fileSpec recurse (fun f -> FileInfo(f).Attributes <- FileAttributes.Normal) 
+    ///<summary>
+    ///Set all files and directories which match a given spec to read and write
+    ///</summary>
+    ///<param name="baseDir">Directory to start looking for files in</param>
+    ///<param name="fileSpec">File mask to match</param>
+    ///<param name="recurse">Should the operation recurse into subdirectories or not</param>
+    let SetFilesToReadWrite baseDir fileSpec recurse =
+        FileOperation baseDir fileSpec recurse (fun f -> FileInfo(f).Attributes <- FileAttributes.Normal) 
 
-        ///<summary>
-        ///Run a process under a different user ID
-        ///</summary>
-        ///<param name="userID">User ID to start process under</param>
-        ///<param name="exeToRun">Name of the executable to run under the different user id</param>
-        ///<param name="arguments">Arguments to the executable</param>
-        let RunProcessAs userID password exeToRun arguments = 
-            let pStartInfo = new ProcessStartInfo(exeToRun,arguments)
-            pStartInfo.UserName <- userID
-            pStartInfo.Password <- password
-            try
-                let p = Process.Start(pStartInfo).WaitForExit
-                (true,Some(p))
-            with
-            | :? SecurityException -> (false, None)
+    ///<summary>
+    ///Run a process under a different user ID
+    ///</summary>
+    ///<param name="userID">User ID to start process under</param>
+    ///<param name="exeToRun">Name of the executable to run under the different user id</param>
+    ///<param name="arguments">Arguments to the executable</param>
+    let RunProcessAs userID password exeToRun arguments = 
+        let pStartInfo = new ProcessStartInfo(exeToRun,arguments)
+        pStartInfo.UserName <- userID
+        pStartInfo.Password <- password
+        try
+            let p = Process.Start(pStartInfo).WaitForExit
+            (true,Some(p))
+        with
+        | :? SecurityException -> (false, None)
 
